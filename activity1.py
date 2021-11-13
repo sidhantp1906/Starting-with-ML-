e48d187f5aeafd21ea0944684dc126807f228ece
Sidhant priyadarshi <60102705+sidhantp1906@users.noreply.github.com>
2021-11-13 19:15:28 +0530

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -55,6 +55,37 @@ standard_deviation = math.sqrt(variance)
 print(f'standard_deviation = {standard_deviation}')
 
 
+sumx = 0
+for j in range(170):
+    sumx = sumx + x[j]
+
+meanx = sumx/170
+print(f'meanx = {meanx}')
+
+nby2x = x[85]
+nby21x = x[86]
+medianx = (nby2x+nby21x)/2
+print(f'medianx = {medianx}')
+
+modex = max(x, key=x.count)
+print(f'modex = {modex}')
+
+x2 = []
+for k in range(170):
+    v3x = x[i]-mean
+    v4x = v3x**2
+    x2.append(v4x)
+print(f'x2 = {x2}')
+
+sum2x = 0
+for l in range(170):
+    sum2x = sum2x + y2[l]
+variancex = sum2x/170
+print(f'variancex = {variancex}')
+
+standard_deviationx = math.sqrt(variancex)
+print(f'standard_deviationx = {standard_deviationx}')
+
 values = Reference(sheet,min_row = 1,max_row = sheet.max_row,min_col = 2,max_col = 3 )
 chart = BarChart()
 chart.add_data(values)
@@ -64,6 +95,11 @@ sheet.add_chart(chart,'e2')
 wb.save('activity1.xlsx')
 plt.scatter(x,y)
 plt.show()
+
+
+
+
+
 
 
 
